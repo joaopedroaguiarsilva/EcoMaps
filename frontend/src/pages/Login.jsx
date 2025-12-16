afcb88e5f3f1a75776b184eb61d01c5bc23f487b
João Pedro Aguiar Silva <joaopedroaguiarsilva8@gmail.com>
2025-12-16 18:17:52 -0300

    feat: adicionando estilo e imagem na tela de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,59 +2,62 @@ import { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import bgImage from "../assets/login-bg.jpg";
+
 
 function Login() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Dados de Login:", { email, senha });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ email, senha });
   }
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h1>Acesse o EcoMaps 🌱</h1>
+    <div
+      className="login-background"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h1>Faça seu login<span>.</span></h1>
 
-        <div className="input-field">
-          <input
-            type="email"
-            placeholder="E-mail"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <FaUser className="icon" />
-        </div>
+          <div className="input-field">
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <FaUser className="icon" />
+          </div>
 
-        <div className="input-field">
-          <input
-            type="password"
-            placeholder="Senha"
-            required
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-          />
-          <FaLock className="icon" />
-        </div>
+          <div className="input-field">
+            <input
+              type="password"
+              placeholder="Senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+            <FaLock className="icon" />
+          </div>
 
-        <div className="recall-forget">
-          <label>
-            <input type="checkbox" />
-            Lembre de mim
-          </label>
-          <a href="#">Esqueceu sua senha?</a>
-        </div>
+          <div className="recall-forget">
+            <a href="#">Esqueci minha senha</a>
+          </div>
 
-        <button type="submit">Login</button>
+          <button type="submit">Entrar</button>
 
-        <div className="signup-link">
-          <p>
-            Não tem uma conta? <Link to="/register">Registrar</Link>
-          </p>
-        </div>
-      </form>
+          <div className="signup-link">
+            <p>
+              Ainda não tenho uma conta? <Link to="/register">Registrar</Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
