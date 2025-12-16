604da88e7d438e1ae13ca45db034556335e1e259
João Pedro Aguiar Silva <joaopedroaguiarsilva8@gmail.com>
2025-12-16 17:54:57 -0300

    feat: adicionando template de tela de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,39 +1,59 @@
 import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("Email:", email);
-    console.log("Senha:", senha);
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Dados de Login:", { email, senha });
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-
+    <div className="container">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label><br />
+        <h1>Acesse o EcoMaps 🌱</h1>
+
+        <div className="input-field">
           <input
             type="email"
+            placeholder="E-mail"
+            required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <FaUser className="icon" />
         </div>
 
-        <div>
-          <label>Senha</label><br />
+        <div className="input-field">
           <input
             type="password"
+            placeholder="Senha"
+            required
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
+          <FaLock className="icon" />
         </div>
 
-        <button type="submit">Entrar</button>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <Link to="/register">Registrar</Link>
+          </p>
+        </div>
       </form>
     </div>
   );
