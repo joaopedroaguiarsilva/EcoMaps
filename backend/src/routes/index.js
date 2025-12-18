b668557c1bb9a4aba41dfc7d912477c2e8e1c7e5
Victor Lopes <victorlopesteodoro@gmail.com>
2025-12-18 19:38:41 -0300

    feat: backend auth

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const exampleController = require('../controllers/exampleController');
 
-router.get('/hello', exampleController.hello);
+const authRoutes = require('./auth.routes');
+
+router.use('/auth', authRoutes);
 
 module.exports = router;
